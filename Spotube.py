50c9ea782a4661b1ef16005fad4856f41d6e69ff
Yan Chuiko <yancomando69@gmail.com>
2023-05-31 17:46:55 +0100

    A bit change buttons

--- Spotube.py
+++ Spotube.py
@@ -87,10 +87,10 @@ label.place(relx=0.5, rely=0.48, anchor=CENTER)
 spotify_user_id_entry = Entry(window, font=("Comic Sans MS", 20), bg="#F0FCFF", justify="center")
 spotify_user_id_entry.place(relx=0.5, rely=0.58, anchor=CENTER)
 
-buttonSubmit = Button(window, text="Sumbit", font=("Comic Sans MS", 19), bg="#FFFFFF", fg="#000000", width=10, height=1, command=convert_playlist)
-buttonSubmit.place(relx=0.5, rely=0.77, anchor=CENTER)
+buttonSubmit = Button(window, text="Sumbit", font=("Comic Sans MS", 18), bg="#FFFFFF", fg="#000000", width=10, height=1, command=convert_playlist)
+buttonSubmit.place(relx=0.5, rely=0.81, anchor=CENTER)
 
-buttonExit = Button(window, text="Exit", font=("Comic Sans MS", 19), bg="#FFFFFF", fg="#000000", width=10, height=1, command=window.destroy)
-buttonExit.place(relx=0.5, rely=0.89, anchor=CENTER)
+buttonExit = Button(window, text="Exit", font=("Comic Sans MS", 18), bg="#FFFFFF", fg="#000000", width=10, height=1, command=window.destroy)
+buttonExit.place(relx=0.5, rely=0.92, anchor=CENTER)
 
 window.mainloop()
