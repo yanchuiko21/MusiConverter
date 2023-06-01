5ec740f9dec012ce9b7c7a69cbedde464118ef0a
Yan Chuiko <yancomando69@gmail.com>
2023-06-01 13:53:02 +0100

    Add some error message

--- Spotube.py
+++ Spotube.py
@@ -1,4 +1,5 @@
 from tkinter import *  # GUI
+from tkinter import messagebox # Error messages
 import spotipy  # Spotify API
 from googleapiclient.discovery import build  # YouTube API
 from spotipy.oauth2 import SpotifyOAuth  # Authenticating requests
@@ -48,19 +49,40 @@ def create_spotify_playlist(spotify_user_id, track_uris):
 
 # Converting the YouTube playlist to a Spotify playlist
 def convert_playlist():
-    youtube_playlist_id = youtube_playlist_id_entry.get()
-    spotify_user_id = spotify_user_id_entry.get()
-    youtube_playlist = get_youtube_playlist(youtube_playlist_id)
-    spotify_uris = []
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope='playlist-modify-public', client_id="612210be399a424788083d9bcb443bfd", client_secret="abb9b871ac1d4742b04b678285a1834f", redirect_uri="http://localhost:8888/callback"))
-    for song, artist in youtube_playlist:
-        artist = artist.replace(" - Topic", "")
-        query = f"track:{song} artist:{artist}"
-        results = sp.search(q=query, limit=1)
-        if results['tracks']['items']:
-            track = results['tracks']['items'][0]
-            spotify_uris.append(track['uri'])
-    create_spotify_playlist(spotify_user_id, spotify_uris)
+    try:
+        youtube_playlist_id = youtube_playlist_id_entry.get()
+        spotify_user_id = spotify_user_id_entry.get()
+        # Checking if the YouTube playlist ID is empty
+        if not youtube_playlist_id:
+            messagebox.showerror("Error", "YouTube Music playlist ID is empty. Please check the ID and try again.")
+            return
+        # Checking if the Spotify user ID is empty
+        if not spotify_user_id:
+            messagebox.showerror("Error", "Spotify user ID is empty. Please check the ID and try again.")
+            return
+        try:
+            youtube_playlist = get_youtube_playlist(youtube_playlist_id)
+        except Exception as e:
+            messagebox.showerror("Error", "Invalid YouTube playlist ID. Please check the ID and try again.")
+            return
+        youtube_playlist = get_youtube_playlist(youtube_playlist_id)
+        spotify_uris = []
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope='playlist-modify-public', client_id="612210be399a424788083d9bcb443bfd", client_secret="abb9b871ac1d4742b04b678285a1834f", redirect_uri="http://localhost:8888/callback"))
+        for song, artist in youtube_playlist:
+            artist = artist.replace(" - Topic", "")
+            query = f"track:{song} artist:{artist}"
+            results = sp.search(q=query, limit=1)
+            if results['tracks']['items']:
+                track = results['tracks']['items'][0]
+                spotify_uris.append(track['uri'])
+        try:
+            create_spotify_playlist(spotify_user_id, spotify_uris)
+            messagebox.showinfo("Successful", "The YouTube Music playlist has been converted to a Spotify playlist.")
+        except Exception:
+            messagebox.showerror("Error", "Invalid Spotify user ID. Please check the ID and try again.")
+            return
+    except Exception as e:
+        messagebox.showerror("Error", "Unknown Error")
 
 # GUI
 window = Tk()
@@ -93,6 +115,13 @@ buttonSubmit.place(relx=0.5, rely=0.81, anchor=CENTER)
 buttonExit = Button(window, text="Exit", font=("Comic Sans MS", 18), bg="#FFFFFF", fg="#000000", width=10, height=1, command=window.destroy)
 buttonExit.place(relx=0.5, rely=0.92, anchor=CENTER)
 
-#TODO: Add Error Handling. Make this project a solid one. Update design of GUI.
+#TODO:
+# Message: Empty YouTube playlist ID entry error message - Done
+# Message: Empty Spotify user ID entry error message - Done
+# Message: YouTube playlist ID entry error message
+# Message: Spotify user ID entry error message
+# Message: YouTube API error message
+# Message: Spotify API error message
+# Message: Converation process complete message - Done
 
 window.mainloop()
