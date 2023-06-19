09c3f62265010312f7fb16d2d12b514f874880cd
Yan Chuiko <yancomando69@gmail.com>
2023-06-19 15:33:43 +0200

    Finish

--- MusiConverter.py
+++ MusiConverter.py
@@ -12,14 +12,9 @@ def center_window(window, width, height):
     y = (screen_height // 2) - (height // 2)
     window.geometry(f"{width}x{height}+{x}+{y}")
 
-# Getting the YouTube playlist
+# Getting the YouTube Music Playlist
 def get_youtube_playlist(youtube_playlist_id):
-    try:
-        youtube = build('youtube', 'v3', developerKey='AIzaSyAGuywMYWB3M6dKEE8RH_-vUqKaADn1Nlg')
-        youtube.search().list(q='test', part='id', maxResults=1).execute()
-    except Exception as e:
-        messagebox.showerror("Error", "Invalid YouTube API Key. Please check the Key and try again.")
-        raise Exception("Invalid YouTube API Key")
+    youtube = build('youtube', 'v3', developerKey='YOUR_API_KEY')
     all_items = []
     try:
         request = youtube.playlistItems().list(
@@ -27,7 +22,7 @@ def get_youtube_playlist(youtube_playlist_id):
             maxResults=50,
             playlistId=youtube_playlist_id
         )
-        while request is not None:  # iterate until no more pages
+        while request is not None:
             response = request.execute()
             for item in response['items']:
                 video_id = item['snippet']['resourceId']['videoId']
@@ -45,38 +40,34 @@ def get_youtube_playlist(youtube_playlist_id):
     except Exception as e:
         messagebox.showerror("Error", "Invalid YouTube Music Playlist ID. Please check the ID and try again.")
         raise Exception("Invalid YouTube Music Playlist ID")
-        
     return all_items
 
-# Creating the Spotify playlist
+# Creating the Spotify Playlist
 def create_spotify_playlist(spotify_user_id, track_uris):
     scope = 'playlist-modify-public'
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id="612210be399a424788083d9bcb443bfd", client_secret="abb9b871ac1d4742b04b678285a1834f", redirect_uri="http://localhost:8888/callback"))
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id="YOUR_CLIENT_ID", client_secret="YOUR_SECRET_ID", redirect_uri="http://localhost:8888/callback"))
     playlist = sp.user_playlist_create(spotify_user_id, "Playlist")
     chunks = [track_uris[i:i + 100] for i in range(0, len(track_uris), 100)]
     for chunk in chunks:
         results = sp.playlist_add_items(playlist['id'], chunk)
 
-# Converting the YouTube playlist to a Spotify playlist
+# Converting the YouTube Music Playlist to a Spotify Playlist
 def convert_playlist():
     try:
         youtube_playlist_id = youtube_playlist_id_entry.get()
         spotify_user_id = spotify_user_id_entry.get()
-        # Checking if the YouTube playlist ID is empty
         if not youtube_playlist_id:
             messagebox.showerror("Error", "YouTube Music Playlist ID is empty. Please check the ID and try again.")
             return
-        # Checking if the Spotify user ID is empty
         if not spotify_user_id:
             messagebox.showerror("Error", "Spotify User ID is empty. Please check the ID and try again.")
             return
-        # Checking if the YouTube playlist ID is valid
         try:
             youtube_playlist = get_youtube_playlist(youtube_playlist_id)
         except Exception as e:
             return
         spotify_uris = []
-        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope='playlist-modify-public', client_id="612210be399a424788083d9bcb443bfd", client_secret="abb9b871ac1d4742b04b678285a1834f", redirect_uri="http://localhost:8888/callback"))
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope='playlist-modify-public', client_id="YOUR_CLIENT_ID", client_secret="YOUR_SECRET_ID", redirect_uri="http://localhost:8888/callback"))
         for song, artist in youtube_playlist:
             artist = artist.replace(" - Topic", "")
             query = f"track:{song} artist:{artist}"
@@ -84,13 +75,12 @@ def convert_playlist():
             if results['tracks']['items']:
                 track = results['tracks']['items'][0]
                 spotify_uris.append(track['uri'])
-        # Checking if the Spotify ID is valid
         try:
             create_spotify_playlist(spotify_user_id, spotify_uris)
         except Exception:
             messagebox.showerror("Error", "Invalid Spotify User ID. Please check the ID and try again.")
             return
-        messagebox.showinfo("Successful", "The YouTube Music Playlist has been converted to a Spotify playlist.")
+        messagebox.showinfo("Successful", "The YouTube Music Playlist has been converted to a Spotify Playlist.")
     except Exception as e:
         messagebox.showerror("Error", "Unknown Error")
 
@@ -126,5 +116,3 @@ buttonExit = Button(window, text="Exit", font=("Comic Sans MS", 18), bg="#FFFFFF
 buttonExit.place(relx=0.5, rely=0.92, anchor=CENTER)
 
 window.mainloop()
-
-# Message: Spotify API error message - In progress
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # MusiConverter
+MusiConverter is a small project that allows you to convert a YouTube Music playlist to a Spotify playlist. It utilizes the YouTube API and Spotify API for fetching and creating playlists.
+
+# Prerequisites
+To run this project, you need the following:
+1. Python 3.x installed on your system.
+2. Required Python packages: tkinter, spotipy, and googleapiclient.
+3. 
+
+# Installation
+1. Clone or download the project files to your local machine.
+2. Install the required Python packages by running the following command: pip install spotipy google-api-python-client
+
+# Usage
+1. Obtain the required API keys:
+- YouTube Data API key: You need to obtain a YouTube Data API key from the Google Developers Console. Make sure to enable the YouTube Data API for your project.
+- Spotify client ID and secret: Create a new application on the Spotify Developer Dashboard to get the client ID and secret. Add the redirect URI as http://localhost:8888/callback.
+
+2. Replace the placeholders in the code:
+- Replace "YOUR_API_KEY" with your YouTube Data API key in the get_youtube_playlist() function.
+- Replace "YOUR_CLIENT_ID" and "YOUR_SECRET_ID" with your Spotify client ID and secret in the create_spotify_playlist() and convert_playlist() functions.
+
+3. Run the main.py file: python main.py
+- The MusiConverter GUI window will appear.
+- Enter the YouTube Music playlist ID and your Spotify user ID in the respective input fields.
+- Click the "Submit" button to start the conversion process.
+- If the playlist conversion is successful, you will see a success message. Otherwise, an error message will be displayed.
+- You can exit the application by clicking the "Exit" button.
+
+Note: Make sure your system has an active internet connection for the APIs to work properly.
+
+# Icon
+An icon image has been added to the MusiConverter GUI window. You can customize the icon by replacing the placeholder <path_to_icon_file> with the actual file path of your icon image in the main.py file.
+
+# Contributing
+Contributions to this project are welcome. If you find any issues or have suggestions for improvements, please open an issue or submit a pull request on the GitHub repository.
+
+# License
+This project is licensed under the MIT License.
